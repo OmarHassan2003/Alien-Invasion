73345328436023a0f7848831400c318ea8bc9639
omar hassan <pmcfm@outlook.com>
2024-03-11 15:43:35 +0200

    Implemented Stack

--- 2ndTermProject/2ndTermProject.cpp
+++ 2ndTermProject/2ndTermProject.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include "SingleLinkedList.h"
 #include "Queue.h"
+#include "Stack.h"
+
+
 using namespace std;
 
 int main()
 {
 	cout << "The Real Slim Shady" << endl;
+	Stack<int> MyStack;
+	Node<int>* s1 = new Node<int>;
+	int x = 5;
+	s1->Ptr_To_Value = &x;
+	Node<int>* s2 = new Node<int>;
+	int x1 = 6;
+	s2->Ptr_To_Value = &x1;
+	Node<int>* s3 = new Node<int>;
+	int x2 = 7;
+	s3->Ptr_To_Value = &x2;
+	Node<int>* s4 = new Node<int>;
+	int x3 = 8;
+	s4->Ptr_To_Value = &x3;
+	MyStack.Push(s1);
+	MyStack.Push(s2);
+	MyStack.Push(s3);
+	MyStack.Push(s4);
+	MyStack.Print();
+	MyStack.Pop();
+	MyStack.Pop();
+	MyStack.Print();
+	cout << *(MyStack.TopElement()->Ptr_To_Value) << endl;
 }
 
--- 2ndTermProject/Stack.h
+++ 2ndTermProject/Stack.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "SingleLinkedList.h"
 using namespace std;
+
 /************************************Stack Implementation**************************************/
 template<typename T>
 class Stack 
@@ -13,6 +14,91 @@ public:
 	Stack();
 	bool Push(Node<T>*);
 	bool Pop();
-	Node<T>* Top() const;
+	Node<T>* TopElement() const;
+	bool IsEmpty();
+	void Print() const;
 	~Stack();
 };
+
+template<typename T>
+Stack<T>::Stack() {
+	Top = NULL;
+	count = 0;
+}
+
+template<typename T>
+bool Stack<T>::Push(Node<T>* NodeToBeAdded) 
+{
+	if (!NodeToBeAdded) return false;
+	Node<T>* New = new Node<T>;
+	New = NodeToBeAdded;
+	if (!Top) 
+	{
+		Top = New;
+		Top->NextNode = NULL;
+		count++;
+	}
+	else 
+	{
+		New->NextNode = Top;
+		Top = New;
+		count++;
+	}
+	return true;
+}
+
+template<typename T>
+bool Stack<T>::Pop()
+{
+	if (!Top) return false;
+	else
+	{
+		Node<T>* temp = Top;
+		Top = Top->NextNode;
+		delete temp;
+		count--;
+		return true;
+	}
+}
+
+template<typename T>
+Node<T>* Stack<T>::TopElement() const
+{
+	if (!Top) return NULL;
+	Node<T>* temp = Top;
+	return temp;
+}
+
+template<typename T>
+bool Stack<T>::IsEmpty() {
+	if (!count) return true;
+	else return false;
+}
+
+template<typename T>
+Stack<T>::~Stack()
+{
+	Node<T>* temp = Top;
+	while (Top) 
+	{
+		Top = Top->NextNode;
+		delete temp;
+		temp = Top;
+	}
+	count = 0;
+}
+
+template<typename T>
+void Stack<T>::Print() const
+{
+	Node<T>* temp = Top;
+	if (!count) {
+		cout << "NO ELEMENTS !" << endl;
+		return;
+	}
+	while (temp != NULL) {
+		cout << *(temp->Ptr_To_Value) << '\t';
+		temp = temp->NextNode;
+	}
+	cout << endl;
+}
