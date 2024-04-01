1c08544084e50f9259739dcfd90b9b9e09d815ae
Ali Alaa <alieldinalaa04@gmail.com>
2024-04-01 23:14:08 +0200

    double Queue

--- 2ndTermProject/DoubleLinkedQueue.h
+++ 2ndTermProject/DoubleLinkedQueue.h
@@ -30,18 +30,13 @@ inline DoubleLinkedQueue<T>::DoubleLinkedQueue()
 template<class T>
 inline bool DoubleLinkedQueue<T>::isEmpty() const
 {
-	return !count;
+	return (Front == nullptr);
 }
 
 template<class T>
 bool DoubleLinkedQueue<T>::enqueue(const T& newEntry)
 {
-	if (!newEntry)
-		return false;
-
-	DoubleNode<T>* New = new Node<T>;
-	New->setItem(newEntry);
-
+	DoubleNode<T>* New = new Node<T>(newEntry);
 	if (!count)
 		Front = New;
 	else
@@ -59,14 +54,18 @@ bool DoubleLinkedQueue<T>::dequeue(T& FrontEntry)
 {
 	if (!count)
 		return false;
-	else
-	{
-		FrontEntry = Front->getItem();
-		Front = Front->getNext();
-		Front->setPrev(nullptr);
-		count--;
-		return true;
-	}
+
+	Node<T>* Del = Front;
+	FrontEntry = Front->getItem();
+	Front = Front->getNext();
+	Front->setPrev(nullptr);
+
+	if (Front == nullptr)
+		Rear = nullptr;
+
+	delete Del;
+	count--;
+	return true;
 }
 
 template<class T>
@@ -91,7 +90,7 @@ inline void DoubleLinkedQueue<T>::print() const
 	}
 	while (temp != nullptr) {
 		cout << *(temp->getItem()) << '\t';//should be changed to the armyunit details
-		temp = temp->NextNode;
+		temp = temp->getNext();
 	}
 	cout << endl;
 }
