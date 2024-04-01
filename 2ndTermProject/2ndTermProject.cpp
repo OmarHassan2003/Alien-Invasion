98ee5faeb3467bd3a1f1a845e25f1c93648fe696
Ali Alaa <alieldinalaa04@gmail.com>
2024-04-01 23:51:34 +0200

    test2 ds

--- 2ndTermProject/2ndTermProject.cpp
+++ 2ndTermProject/2ndTermProject.cpp
@@ -1,6 +1,6 @@
 #include "Stack/Stack.h"
 #include "Queue/Queue.h"
-#include "DoubleLinkedQueue.h"
+#include "Queue/DoubleLinkedQueue.h"
 //#include "priQueue.h"
 
 #include <fstream>
@@ -10,11 +10,30 @@ int main()
 	Stack<int> S1;
 	Queue<int> Q1;
 	DoubleLinkedQueue<int> D1;
-	//priQueue<int> P1;
-
+	int s, q, d;
 	S1.push(8);
 	S1.print();
+	S1.peek(s);
+	cout << s << endl;
+	S1.pop(s);
+	cout << S1.isEmpty() << endl;
+	S1.print();
+
 	Q1.enqueue(6);
 	Q1.print();
+	Q1.peek(q);
+	cout << q << endl;
+	Q1.dequeue(q);
+	cout << Q1.isEmpty() << endl;
+	Q1.print();
+
+
+	D1.enqueue(4);
+	D1.print();
+	D1.peek(d);
+	cout << d << endl;
+	D1.dequeue(d);
+	cout << Q1.isEmpty() << endl;
+	D1.print();
 }
 
