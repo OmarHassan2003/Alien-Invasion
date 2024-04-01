c152a841cc309a48ba4376d107f553df328287d7
Ali Alaa <alieldinalaa04@gmail.com>
2024-04-01 23:39:31 +0200

    test ds

--- 2ndTermProject/2ndTermProject.cpp
+++ 2ndTermProject/2ndTermProject.cpp
@@ -1,10 +1,18 @@
 #include "Stack/Stack.h"
 #include "Queue/Queue.h"
-
+#include "DoubleLinkedQueue.h"
+//#include "priQueue.h"
 
 int main()
 {
 	Stack<int> S1;
-	Queue<int> Q1;	
+	Queue<int> Q1;
+	DoubleLinkedQueue<int> D1;
+	//priQueue<int> P1;
+
+	S1.push(8);
+	S1.print();
+	Q1.enqueue(6);
+	Q1.print();
 }
 
