03575ff71b148a80ec6f4f49e82541afe1b130e6
Mohamed Bahgat <mbahgat503@gmail.com>
2024-04-02 20:20:17 +0200

    Update

--- 2ndTermProject/RandomGenerator/RandomGenerator.cpp
+++ 2ndTermProject/RandomGenerator/RandomGenerator.cpp
@@ -1,1 +1,80 @@
 #include "RandomGenerator.h"
+
+RandomGenerator::RandomGenerator()
+{
+	cout << "Random Generator Class has been started" << endl;
+	Read();
+}
+
+void RandomGenerator::Read()
+{
+	ifstream Fin("../test.txt");
+	if (Fin.is_open())
+	{
+		Fin >> n >> ES >> ET >> EG >> AS >> AM >> AD >> Prop;
+		/***********************Read Earth Data*************************/
+		Fin >> min_E_Power;
+		Fin.ignore();
+
+		Fin >> max_E_Power;
+		Fin.ignore();
+
+		Fin >> min_E_health;
+		Fin.ignore();
+
+		Fin >> max_E_health;
+		Fin.ignore();
+
+		Fin >> min_E_Attack_Capacity;
+		Fin.ignore();
+
+		Fin >> max_E_Attack_Capacity;
+		Fin.ignore();
+		/***********************Read Earth Data*************************/
+		/***********************Read ALien Data*************************/
+		Fin >> min_A_Power;
+		Fin.ignore();
+
+		Fin >> max_A_Power;
+		Fin.ignore();
+
+		Fin >> min_A_health;
+		Fin.ignore();
+
+		Fin >> max_A_health;
+		Fin.ignore();
+
+		Fin >> min_A_Attack_Capacity;
+		Fin.ignore();
+
+		Fin >> max_A_Attack_Capacity;
+		Fin.ignore();
+		/***********************Read ALien Data*************************/
+		Fin.close();
+	}
+	else
+		cout << "File failed" << endl;
+}
+
+inline unsigned short RandomGenerator::RandGen(unsigned short lower_bound, unsigned short upper_bound)
+{
+	return lower_bound + rand() % (upper_bound - lower_bound + 1);
+}
+
+void RandomGenerator::Print()
+{
+	cout << n << " " << ES << " " << ET << " " << EG << " " << AS << " " << AM << " " << AD << " " << Prop << endl;
+	cout << min_E_Power << endl;
+	cout << max_E_Power << endl;
+	cout << min_E_health << endl;
+	cout << max_E_health << endl;
+	cout << min_E_Attack_Capacity << endl;
+	cout << max_E_Attack_Capacity << endl;
+
+	cout << min_A_Power << endl;
+	cout << max_A_Power << endl;
+	cout << min_A_health << endl;
+	cout << max_A_health << endl;
+	cout << min_A_Attack_Capacity << endl;
+	cout << max_A_Attack_Capacity << endl;
+}
--- 2ndTermProject/RandomGenerator/RandomGenerator.h
+++ 2ndTermProject/RandomGenerator/RandomGenerator.h
@@ -14,76 +14,9 @@ protected:
 	unsigned short min_A_Power, min_A_health, min_A_Attack_Capacity;
 	unsigned short max_A_Power, max_A_health, max_A_Attack_Capacity;
 public:
-	RandomGenerator():n(0)
-	{
-		cout << "Random Generator Class has been started" << endl;
-		Read();
-	}
-	void Read()
-	{
-		ifstream Fin("../test.txt");
-		if (Fin.is_open())
-		{
-			Fin >> n >> ES >> ET >> EG >> AS >> AM >> AD >> Prop;
-			/***********************Read Earth Data*************************/
-			Fin >> min_E_Power;
-			Fin.ignore();
-
-			Fin >> max_E_Power;
-			Fin.ignore();
-
-			Fin >> min_E_health;
-			Fin.ignore();
-
-			Fin >> max_E_health;
-			Fin.ignore();
-
-			Fin >> min_E_Attack_Capacity;
-			Fin.ignore();
-
-			Fin >> max_E_Attack_Capacity;
-			Fin.ignore();
-			/***********************Read Earth Data*************************/
-			/***********************Read ALien Data*************************/
-			Fin >> min_A_Power;
-			Fin.ignore();
-
-			Fin >> max_A_Power;
-			Fin.ignore();
-
-			Fin >> min_A_health;
-			Fin.ignore();
-
-			Fin >> max_A_health;
-			Fin.ignore();
-
-			Fin >> min_A_Attack_Capacity;
-			Fin.ignore();
-
-			Fin >> max_A_Attack_Capacity;
-			Fin.ignore();
-			/***********************Read ALien Data*************************/
-			Fin.close();
-		}
-		else
-			cout << "File failed" << endl;
-	}
-	void Print()
-	{
-		cout << n << " " << ES << " " << ET << " " << EG << " " << AS << " " << AM << " " << AD << " " << Prop << endl;
-		cout << min_E_Power << endl;
-		cout << max_E_Power << endl;
-		cout << min_E_health << endl;
-		cout << max_E_health << endl;
-		cout << min_E_Attack_Capacity << endl;
-		cout << max_E_Attack_Capacity << endl;
-
-		cout << min_A_Power << endl;
-		cout << max_A_Power << endl;
-		cout << min_A_health << endl;
-		cout << max_A_health << endl;
-		cout << min_A_Attack_Capacity << endl;
-		cout << max_A_Attack_Capacity << endl;
-	}
+	RandomGenerator();
+	void Read();
+	inline unsigned short RandGen(unsigned short lower_bound, unsigned short upper_bound);
+	void Print();
 };
 
