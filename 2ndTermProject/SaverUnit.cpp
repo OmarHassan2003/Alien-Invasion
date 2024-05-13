de02792c6f095a314ffbd29c6a98b536b8a8a8d7
Ali Alaa <alieldinalaa04@gmail.com>
2024-05-14 00:08:54 +0300

    SU attack

--- 2ndTermProject/SaverUnit.cpp
+++ 2ndTermProject/SaverUnit.cpp
@@ -1,4 +1,6 @@
 #include "SaverUnit.h"
+#include "AlienArmy/AlienSoldier.h"
+#include "Game.h"
 
 SaverUnit::SaverUnit(Game* p, int HP, int pow, int ID_, int cap, int _Tj, Unit U)
 	:ArmyUnit(p, HP, pow, ID_, cap, _Tj, U)
@@ -7,7 +9,40 @@ SaverUnit::SaverUnit(Game* p, int HP, int pow, int ID_, int cap, int _Tj, Unit U
 
 bool SaverUnit::Attack()
 {
-	return false;
+	bool flag = false;
+	AlienSoldier* AS = nullptr;
+	Queue<AlienSoldier*> templist;
+	for (int i = 0; i < GetAttackCap(); i++)
+		if (pGame->Get_AS(AS))
+		{
+			flag = true;
+			if (AS->Get_Ta() == -1)
+				AS->Set_Ta(pGame->Get_Tj());
+
+			int dmg = int((float)GetPower() * (GetHealth() / 100.0) / (float)sqrt(AS->GetHealth()));
+			AS->SetHealth(AS->GetHealth() - dmg);
+			templist.enqueue(AS);
+		}
+		else
+			break;
+
+	if (pGame->Get_GameMode())
+	{
+		if (!templist.isEmpty())
+		{
+			cout << "ES " << this << " shots ";
+			templist.print();
+			cout << endl;
+		}
+	}
+	while (templist.dequeue(AS))
+	{
+		if (AS->GetHealth() <= 0)
+			pGame->AddInKilledList(AS);
+		else
+			pGame->Add_AS(AS);
+	}
+	return flag;
 }
 
 ostream& operator<<(ostream& COUT, SaverUnit* Passed_AU)
